12f7b42af4275e6093e86b2ff450494126d45f72
Miguel <miguel.cordero.lugo@hotmail.com>
2025-05-30 19:40:49 +0200

    commit1

---
+++ pruebaGit/src/pruebagit/PruebaGit.java
@@ -0,0 +1,11 @@
+package pruebagit;
+
+ 
+public class PruebaGit {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println(Sumador.sumar(15f, 4f));
+    }
+    
+}
---
+++ pruebaGit/src/pruebagit/Sumador.java
@@ -0,0 +1,8 @@
+package pruebagit;
+
+
+public class Sumador {
+    public static float sumar (float a, float b ) { 
+	return a + b; 
+    }
+}
